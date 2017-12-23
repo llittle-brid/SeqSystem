26f882b0c02e04afe0e714d63324900fb2b3d3fa
Zakfung <feng_r1ght@qq.om>
2017-12-23 19:27:40 +0800

    Merge branch 'master' of https://github.com/llittle-brid/SeqSystem
    # Conflicts:
    #	src/main/webapp/login.jsp

--- src/main/java/action/LibrarycollectAction.java
+++ src/main/java/action/LibrarycollectAction.java
@@ -7,6 +7,7 @@ import com.opensymphony.xwork2.Preparable;
 import dao.LibrarycollectDao;
 import daoImp.LibrarycollectDaoImp;
 import entity.LibrarycollectEntity;
+import entity.LibraryEntity;
 import entity.UserEntity;
 import org.apache.struts2.components.If;
 import org.apache.struts2.interceptor.RequestAware;
@@ -22,7 +23,6 @@ public class LibrarycollectAction extends ActionSupport implements RequestAware,
     private Map<String,Object> request;
     private Map<String,Object> session;
     private Map<String, Object> dataMap;
-    private int page;
 
     public String collect() {
         dataMap = new HashMap<String, Object>();
@@ -30,33 +30,6 @@ public class LibrarycollectAction extends ActionSupport implements RequestAware,
         boolean res=librarycollectDao.collect((((UserEntity)session.get("user")).getId_user()),librarycollect.getId_library());
         dataMap.put("res", res);
         return "RES";
-    }
-
-    public String Mycollect() {
-        dataMap = new HashMap<String, Object>();
-        librarycollectDao = new LibrarycollectDaoImp();
-        List<LibrarycollectEntity> librarycollectAll;
-        int page=1;
-        librarycollectAll=librarycollectDao.getMycollect((((UserEntity)session.get("user")).getId_user()),(page-1)*6,(page-1)*6+6);
-        ActionContext.getContext().getValueStack().set("list",librarycollectAll);
-        int count=librarycollectDao.getAllcount((((UserEntity)session.get("user")).getId_user()));
-        int num=count/6+1;
-        request.put("num",num);
-        request.put("page",page);
-        return "getMycollect";
-    }
-
-    public String Mycollectagain(){
-        dataMap = new HashMap<String, Object>();
-        librarycollectDao = new LibrarycollectDaoImp();
-        List<LibrarycollectEntity> librarycollectAllagain;
-        librarycollectAllagain=librarycollectDao.getMycollect((((UserEntity)session.get("user")).getId_user()),(page-1)*6,(page-1)*6+6);
-        ActionContext.getContext().getValueStack().set("list",librarycollectAllagain);
-        int count=librarycollectDao.getAllcount((((UserEntity)session.get("user")).getId_user()));
-        int num=count/6+1;
-        request.put("num",num);
-        request.put("page",page);
-        return "getMycollect";
     }
 
 
@@ -89,9 +62,5 @@ public class LibrarycollectAction extends ActionSupport implements RequestAware,
         this.dataMap = dataMap;
     }
 
-
-    public void setPage(int page) {
-        this.page = page;
-    }
 
 }
--- src/main/java/dao/LibrarycollectDao.java
+++ src/main/java/dao/LibrarycollectDao.java
@@ -6,7 +6,4 @@ public interface LibrarycollectDao {
     boolean collect(int id_user,int id_library);
     LibrarycollectEntity getOne(String name);
     List<LibrarycollectEntity> getAll(int id_user);
-    List<LibrarycollectEntity> getMycollect(int id_user,int num1,int num2);
-
-    int getAllcount(int id_user);
 }
