cf961ab941f7bff39f92ec954d45d8586f12d3ad
Zakfung <feng_r1ght@qq.om>
2017-12-29 20:50:42 +0800

    Merge branch 'master' of https://github.com/llittle-brid/SeqSystem
    # Conflicts:
    #	src/main/webapp/login.jsp

--- src/main/java/dao/StructureDao.java
+++ src/main/java/dao/StructureDao.java
@@ -3,6 +3,5 @@ import java.util.List;
 import entity.StructureEntity;
 public interface StructureDao {
     List<StructureEntity> getAll(int id_library,int num1,int num2);
-    void insert(String toJson);
     int count(int id_library);
 }
