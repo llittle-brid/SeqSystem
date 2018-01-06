6a23686bc5ca0b4679803dbe9c7db042cd3ceced
theodore3131 <alantheodore32@gmail.com>
2018-01-06 14:41:50 +0800

    2018/01/06 xzw 完成恢复

--- src/main/java/daoImp/ProjectDaoImp.java
+++ src/main/java/daoImp/ProjectDaoImp.java
@@ -39,6 +39,7 @@ public class ProjectDaoImp extends DAO<ProjectEntity> implements ProjectDao {
             }
         }
 
+
         UserEntity user = (UserEntity)ActionContext.getContext().getSession().get("user");
         int ID_User = user.getId_user();
 
@@ -83,7 +84,7 @@ public class ProjectDaoImp extends DAO<ProjectEntity> implements ProjectDao {
     @Override
     public boolean alterPM(int idUser, int idProject) {
         String sql = "select count(*) from PROJECT_MEMBER where ID_PROJECT = ? and ID_USER = ?";
-        if (Integer.valueOf(getForValue(sql,idProject,idUser))<1){
+        if (Integer.valueOf(getForValue(sql,idProject,idUser).toString())<1){
             return false;
         }
         else {
@@ -124,7 +125,7 @@ public class ProjectDaoImp extends DAO<ProjectEntity> implements ProjectDao {
 
         String sql1 = "select count(*) from PROJECT_MEMBER where ID_PROJECT = ? and ID_USER = ?";
 
-        if (Integer.valueOf(getForValue(sql1,idProject,idUser))==1) {
+        if (Integer.valueOf(getForValue(sql1,idProject,idUser).toString())==1) {
             return false;
         }
 
@@ -145,7 +146,7 @@ public class ProjectDaoImp extends DAO<ProjectEntity> implements ProjectDao {
 
     public boolean addMember(int idProject, int idUser){
         String sql1 = "select count(*) from PROJECT_MEMBER where ID_PROJECT = ? and ID_USER = ?";
-        if (Integer.valueOf(getForValue(sql1,idProject,idUser))==1) {
+        if (Integer.valueOf(getForValue(sql1,idProject,idUser).toString())==1) {
             return false;
         }
 
