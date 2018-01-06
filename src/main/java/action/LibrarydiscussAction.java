75e124ae8836e8abf84e62a90f135ec3fc076dc2
Zakfung <feng_r1ght@qq.om>
2018-01-06 23:09:43 +0800

    Merge branch 'master' of https://github.com/llittle-brid/SeqSystem
    # Conflicts:
    #	src/main/webapp/login.jsp

--- src/main/java/action/LibrarydiscussAction.java
+++ src/main/java/action/LibrarydiscussAction.java
@@ -7,13 +7,16 @@ import dao.LibrarydiscussDao;
 import daoImp.LibrarydiscussDaoImp;
 import entity.LibrarydiscussEntity;
 import entity.LibrarydiscussEntity;
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.components.If;
 import org.apache.struts2.interceptor.RequestAware;
 import org.apache.struts2.interceptor.SessionAware;
-import java.util.HashMap;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class LibrarydiscussAction extends ActionSupport implements RequestAware, SessionAware, ModelDriven<LibrarydiscussEntity>, Preparable {
     private LibrarydiscussDao librarydiscussDao;
@@ -21,6 +24,16 @@ public class LibrarydiscussAction extends ActionSupport implements RequestAware,
     private Map<String,Object> request;
     private Map<String,Object> session;
     private Map<String, Object> dataMap;
+    private File file;
+    private String fileFileName;
+
+    public void setFileFileName(String fileFileName) {
+        this.fileFileName = fileFileName;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
 
     public String commit() {
         dataMap = new HashMap<String, Object>();
@@ -37,6 +50,41 @@ public class LibrarydiscussAction extends ActionSupport implements RequestAware,
         librarydiscussDao = new LibrarydiscussDaoImp();
         boolean res=librarydiscussDao.delete(librarydiscuss.getId_lib_discuss());
         dataMap.put("res", res);
+        return "RES";
+    }
+
+    public String image()
+    {
+        System.out.println(1111112246);
+        dataMap = new HashMap<String, Object>();
+        if (file!=null) {
+            String savePath = ServletActionContext.getServletContext().getRealPath("disImage");
+
+            SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMdd");
+            String ymd=sdf.format(new Date());
+            savePath+="\\"+ymd+"\\";
+            File dirFile=new File(savePath);
+            if(!dirFile.exists()){
+                dirFile.mkdir();
+            }
+                String fileExt=fileFileName.substring(fileFileName.lastIndexOf(".")+1).trim().toLowerCase();
+                SimpleDateFormat sdfForFileName=new SimpleDateFormat("yyyyMMddHHmmss");
+                String newName=sdfForFileName.format(new Date())+"_"+new Random().nextInt(1000)+"."+fileExt;
+                File destFile=new File(dirFile,newName);
+
+                try {
+                    System.out.println("Src File name: " + file);
+                    System.out.println("Dst File name: " + fileFileName);
+//                    File destFile = new File(savePath, MyFileFileName.get(i));
+                    FileUtils.copyFile(file, destFile);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String path=savePath+newName;
+                dataMap.put("path",path);
+            System.out.println(path);
+        }
         return "RES";
     }
 
