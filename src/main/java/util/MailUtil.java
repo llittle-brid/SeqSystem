b33b9d497609e3f3c7a16186a04d8957f60331dc
Zakfung <feng_r1ght@qq.om>
2018-01-08 21:08:24 +0800

    Merge branch 'master' of https://github.com/llittle-brid/SeqSystem
    # Conflicts:
    #	src/main/webapp/login.jsp

--- src/main/java/util/MailUtil.java
+++ src/main/java/util/MailUtil.java
@@ -53,41 +53,7 @@ public class MailUtil {
         Transport.send(message);
     }
 
-//    public static boolean sslSend(MessageInfo msg1, EmailAccount emailAccount)
-//            throws AddressException, MessagingException, IOException{
-//        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-//        // Get a Properties object
-//        Properties props = new Properties();
-//        props.setProperty("mail.smtp.host", emailAccount.getPlace());
-//
-//        props.setProperty("mail.smtp.port", "465");
-//        props.setProperty("mail.smtp.socketFactory.port", "465");
-//        props.put("mail.smtp.auth", "true");
-//
-//        final String username = emailAccount.getUsername();
-//        final String password = emailAccount.getPassword();
-//        Session session = Session.getDefaultInstance(props, new Authenticator(){
-//            protected PasswordAuthentication getPasswordAuthentication() {
-//                return new PasswordAuthentication(username, password);
-//            }});
-//        Message msg = new MimeMessage(session);
-//
-//        // 设置发件人和收件人
-//        msg.setFrom(new InternetAddress(emailAccount.getUsername()));
-//        List<String> tos = msg1.getTo();
-//        Address to[] = new InternetAddress[tos.size()];
-//        for(int i=0;i<tos.size();i++){
-//            to[i] = new InternetAddress(tos.get(i));
-//        }
-//        // 多个收件人地址
-//        msg.setRecipients(Message.RecipientType.TO, to);
-//        msg.setSubject(msg1.getSubject()); // 标题
-//        msg.setText(msg1.getMsg());// 内容
-//        msg.setSentDate(new Date());
-//        Transport.send(msg);
-//        System.out.println("EmailUtil ssl协议邮件发送打印" +msg.toString());
-//        return true;
-//    }
+
 
     private static Message getMessage(postmailEntity info) throws Exception{
         final Properties p = System.getProperties() ;
